e0479f05abd635bf31ef263cde5ac287c025554b
Eduard <kozeychukk1997@gmail.com>
2024-06-23 15:16:12 +0300

    react-basic(HomeWork)

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -9,6 +9,9 @@ export const Counter = () => {
     //     console.log(count)
     // }
 
+    // Завдання:
+    // - Додати кнопку Reset Count для компоненту Count.tsx
+
     const [count, setCount] = useState(0);
     const counterOptions = [10, 20, 30, 40, 50, -50, -40, -30, -20, -10];
 
@@ -18,21 +21,30 @@ export const Counter = () => {
 
     const decrease = () => {
         setCount(count - 1);
-    }
+    };
 
     const changeCounter = (newCount: number) => setCount(newCount);
-    
+
+    const resetCount = () => {
+        setCount(0)
+    }
+
     return (
         <div>
             <h2> Counter {count} </h2>
             <button onClick={increase}>increase</button>
             <button onClick={decrease}>decrease</button>
 
+            <button onClick={resetCount}>Reset Count</button>
+
             {counterOptions.map((countOption) => (
-                <button key={countOption} onClick={() => changeCounter(countOption)}>
+                <button
+                    key={countOption}
+                    onClick={() => changeCounter(countOption)}
+                >
                     {countOption}
                 </button>
-      ))}
+            ))}
         </div>
     );
 };
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,18 +2,34 @@ import React, { useState } from "react";
 
 export const LoginForm = () => {
     // two-way-binding
+
+    // Завдання:
+    // Додати state для password input у компоненті LoginForm
     const [email, setEmail] = useState("");
 
-    const onSubmit = () => {
-        console.log("Email",`${email}`)
-    }
+    const [password, setPassword] = useState("");
 
-    console.log(email);
+    const onSubmit = () => {
+        console.log("Email", `${email}`);
+        console.log("Paswword", `${password}`);
+    };
+
+    // console.log(email);
 
     return (
         <div>
-            <input type='text' placeholder='email' value={email} onChange={(event) => setEmail(event.target.value)}/>
-            <input type='password' placeholder='password' />
+            <input
+                type='text'
+                placeholder='email'
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+            />
+            <input
+                type='password'
+                placeholder='password'
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+            />
             <button onClick={onSubmit}>Submit</button>
         </div>
     );
